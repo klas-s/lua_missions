bee70424e9cccc8614728d30c79153d5b1dedc6a
Klas Stålbröst <11561015+klas-s@users.noreply.github.com>
2025-08-25 19:38:10 +0200

    solved a couple of missions

--- missions/tables.lua
+++ missions/tables.lua
@@ -7,91 +7,92 @@ local unpack = _G.unpack or table.unpack
 
 function test_creating_empty_tables()
   local empty_table = {}
-  assert_equal( __, type(empty_table))
+  assert_equal("table", type(empty_table))
 end
 
 function test_empty_tables_return_nil_when_indexed()
   local empty_table = {}
-  assert_equal( __, empty_table[1])
+  assert_equal(nil, empty_table[1])
 end
+
 
 function test_table_modifications()
   local t = {}
   t[1] = 1
-  assert_equal(__, t[1])
+  assert_equal(1, t[1])
 
   t[2] = 2
-  assert_equal(__, t[2])
+  assert_equal(2, t[2])
 
   t[3] = 'a'
-  assert_equal(__, t[3])
+  assert_equal('a', t[3])
 
-  assert_equal(__, t[4])
+  assert_equal(nil, t[4])
 end
 
 function test_inline_table_literals()
   local t = { 1, 2, 'a' }
-  assert_equal(__, t[1])
-  assert_equal(__, t[2])
-  assert_equal(__, t[3])
+  assert_equal(1, t[1])
+  assert_equal(2, t[2])
+  assert_equal('a', t[3])
 end
 
 function test_tables_inside_tables()
   local t = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } }
-  assert_equal(__, t[2][3])
+  assert_equal(6, t[2][3])
 end
 
 function test_tables_are_references_not_values()
   local a = { 1, 2, 3 }
   local b = { 1, 2, 3 }
   local c = a
-  assert_equal(__, a == b)
-  assert_equal(__, a == c)
+  assert_equal(false, a == b)
+  assert_equal(true, a == c)
 end
 
 function test_table_concat()
   local t = { 'one', 'two', 'three' }
-  assert_equal(__, table.concat(t))
+  assert_equal("onetwothree", table.concat(t))
 end
 
 function test_table_concat_with_separator()
   local t = { 'one', 'two', 'three' }
-  assert_equal(__, table.concat(t, ', ') )
+  assert_equal("one, two, three", table.concat(t, ', ') )
 end
 
 function test_the_default_table_table()
   -- there's a table called "table". It has functions for table manipulation inside
-  assert_equal( __, type(table))
+  assert_equal( "table", type(table))
 end
 
 function test_table_insert()
   local a = { 1, 2 }
   table.insert(a, 3)
-  assert_equal(__, table.concat(a, ', '))
+  assert_equal("1, 2, 3", table.concat(a, ', '))
 end
 
 function test_table_insert_with_position()
   local a = { 'a', 'c' }
   table.insert(a, 2, 'b')
-  assert_equal(__, table.concat(a, ', '))
+  assert_equal("a, b, c", table.concat(a, ', '))
 end
 
 function test_table_remove()
   local a = { 1, 2, 3, 'last' }
   table.remove(a)
-  assert_equal(__, table.concat(a, ', '))
+  assert_equal("1, 2, 3", table.concat(a, ', '))
 end
 
 function test_table_remove_with_position()
   local a = { 'a', 'b', 'foo', 'c' }
   table.remove(a, 3)
-  assert_equal(__, table.concat(a, ', '))
+  assert_equal("a, b, c", table.concat(a, ', '))
 end
 
 function test_table_sort()
   local x = { 5, 3, 25, 1 }
   table.sort(x)
-  assert_equal(__, table.concat(x, ', '))
+  assert_equal("1, 3, 5, 25", table.concat(x, ', '))
 end
 
 function test_table_sort_with_function()
@@ -146,7 +147,7 @@ end
 
 function test_using_tables_as_keys()
   local hi = { 1, 2, 3 }
-  local you = { 4, 5, 6 } 
+  local you = { 4, 5, 6 }
   local t = { [hi] = 'hello' }
   t[you] = 'world'
   assert_equal(__, t[hi])
